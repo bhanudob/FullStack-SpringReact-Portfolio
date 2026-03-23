e6c6b440f36832faa487c15319b46e78a7c10013
bhanu <bhanud2024@gmail.com>
2026-03-23 01:08:23 -0500

    Chapter 17: Add MySQL driver for Railway deployment

--- src/main/java/com/packt/cardatabase/CardatabaseApplication.java
+++ src/main/java/com/packt/cardatabase/CardatabaseApplication.java
@@ -40,7 +40,7 @@ public class CardatabaseApplication implements CommandLineRunner {
 		Owner owner2 = new Owner("Mary", "Robinson");
 		orepository.saveAll(Arrays.asList(owner1, owner2));
 
-		repository.save(new Car("Ford", "Mustang", "Red", "ADF-1121", 2023, 59000, owner1));
+		repository.save(new Car("Ford", "Mustang", "Blue", "ADF-1121", 2023, 59000, owner1));
 		repository.save(new Car("Nissan", "Leaf", "White", "SSJ-3002", 2020, 29000, owner2));
 		repository.save(new Car("Toyota", "Prius", "Silver", "KKO-0212", 2022, 39000, owner2));
 
